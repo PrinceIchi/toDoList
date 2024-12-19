5a7eb48f6863fd7bb526a9d55aadd82aa70ea4f0
Prince <principe3219@icloud.com>
2024-12-18 16:47:19 -0800

    more practice

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function addTodo() {
-const todo = document.getElementById('new-todo').value;
+    const todoElement = document.getElementById('new-todo');
+    const todo = todoElement.value;
+        todoElement.value = "";
+    if (todo.length < 3) {
+        return alert('you must have at least 3 characters for your todo list')
+    }
+    const list = document.getElementById('list');
+    const li = document.createElement('li');
+    const text = document.createTextNode(todo);
 
+li.setAttribute('onclick', 'this.remove()');
+
+    li.appendChild(text);
+    list.appendChild(li);
 }
